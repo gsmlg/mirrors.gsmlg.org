eecb70b15e1a2115774d434ba64992daefd74288
gsmlg <me@gsmlg.org>
2018-08-09 22:19:28 +0800

    add emacs elpa

--- mirrors.js
+++ mirrors.js
@@ -3,4 +3,7 @@ module.exports = [
    * example:
    * [ 'https://apt.ubuntu.com/reps', '/ubuntu-repo']
    */
+  ['https://elpa.gnu.org/packages', '/gnu-elpa'],
+  ['https://melpa.org/packages', '/melpa'],
+  ['https://orgmode.org/elpa', '/org-elpa'],
 ];
